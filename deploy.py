21be89b5abc3f9d49b68b636675c6f435832b257
Kshitiz Raj <srmkr7800@gmail.com>
2023-03-07 02:31:07 +0530

    debug

--- deploy.py
+++ deploy.py
@@ -1,6 +1,10 @@
 from solcx import compile_standard, install_solc
 import json
 from web3 import Web3
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 with open("./SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
@@ -32,10 +36,10 @@ abi = compiled_sol["contracts"]["simpleStorage.sol"]["SimpleStorage"]["abi"]
 
 #Connecting to Ganache
 w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
-chain_id = 5777
+chain_id = 1337
 
-my_address = "0xDd2a8290fEc9dFD0c2b98841D1AAFcdA3B85B8F9"
-private_key = "0xc1b750e20006223a7d690166cfa42c46470844e9b34638096edc8e61bfb2"
+my_address = "0x6E335E1B6026460e83C4A502dB7bc70798fAD5fe"
+private_key = os.getenv("PRIVATE_KEY")
 
 
 SimpleStorage = w3.eth.contract(abi=abi,bytecode=bytecode)
@@ -43,18 +47,20 @@ SimpleStorage = w3.eth.contract(abi=abi,bytecode=bytecode)
 
 #get latest Transaction
 nonce = w3.eth.get_transaction_count(my_address)
-print(nonce)
+# # print(nonce)
 
-#1. Build a Transaction
-#2. Sign a Transaction
-#3. Send a Transaction
+# #1. Build a Transaction
+# #2. Sign a Transaction
+# #3. Send a Transaction
 
 transaction = SimpleStorage.constructor().build_transaction(
-    {"gasPrice":w3.eth.gas_price,"chainID":chain_id,"from":my_address,"nonce":nonce}
+    {"gasPrice":w3.eth.gas_price,"chainId":chain_id,"from":my_address,"nonce":nonce}
 )
-# print(transaction)
+# # print(transaction)
 
-signed_txn = w3.eth.account.sign_transaction(transaction,private_key)
+signed_txn = w3.eth.account.sign_transaction(transaction,private_key=private_key)
+# print(signed_txn)
 
+tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+tx_recipt= w3.eth.wait_for_transaction_receipt(tx_hash)
 
-# tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
